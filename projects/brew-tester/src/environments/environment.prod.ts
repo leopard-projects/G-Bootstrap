e7a65e1337cdfa825798b1bc8587edb33c0216d5
projects <projectleopard2021@gmail.com>
2021-07-13 16:29:48 -0400

    Fixed images issue

--- projects/brew-tester/src/environments/environment.prod.ts
+++ projects/brew-tester/src/environments/environment.prod.ts
@@ -1,16 +1,28 @@
 export const environment = {
   production: true,
 
+  // api: {
+  //   promotion: '../../w-beta/assets/mock-prod-api/promotion.json',
+  //   carousel: '../../w-beta/assets/mock-prod-api/carousel.json',
+  //   widecontainerone: '../../w-beta/assets/mock-api/wide-container-one.json',
+  //   macbook: '../../../w-beta/assets/images/macbook.png',
+  //   bradelvis: '../../../w-beta/assets/images/brad-elvis.png',
+  //   ipad: '../../../w-beta/assets/images/ipad.png',
+  //   macbook2: '../../../w-beta/assets/images/macbook-2.png',
+  //   jeans: '../../../w-beta/assets/images/jeans.svg',
+  //   computer: '../../../w-beta/assets/images/computer.jpg',
+  //   stage: '../../../w-beta/assets/images/stage.jpg'
+  // }
   api: {
-    promotion: '../../w-beta/assets/mock-prod-api/promotion.json',
-    carousel: '../../w-beta/assets/mock-prod-api/carousel.json',
-    widecontainerone: '../../w-beta/assets/mock-api/wide-container-one.json',
-    macbook: '../../../w-beta/assets/images/macbook.png',
-    bradelvis: '../../../w-beta/assets/images/brad-elvis.png',
-    ipad: '../../../w-beta/assets/images/ipad.png',
-    macbook2: '../../../w-beta/assets/images/macbook-2.png',
-    jeans: '../../../w-beta/assets/images/jeans.svg',
-    computer: '../../../w-beta/assets/images/computer.jpg',
-    stage: '../../../w-beta/assets/images/stage.jpg'
-  }
+    promotion: "../../assets/mock-api/promotion.json",
+    carousel: "../../assets/mock-api/carousel.json",
+    widecontainerone: "../../assets/mock-api/wide-container-one.json",
+    macbook: "../../../assets/images/macbook.png",
+    bradelvis: "../../../assets/images/brad-elvis.png",
+    ipad: "../../../assets/images/ipad.png",
+    macbook2: "../../../assets/images/macbook-2.png",
+    jeans: "../../../assets/images/jeans.svg",
+    computer: "../../../assets/images/computer.jpg",
+    stage: "../../../assets/images/stage.jpg",
+  },
 };
